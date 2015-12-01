2daf428100027c3d5d83e0db767725dd670bb528
Bilal Reza <rezasb@s.dcsdk12.org>
2015-12-01 10:20:19 -0700

    End of day

--- main.rb
+++ main.rb
@@ -2,6 +2,12 @@ require_relative 'pawns'
 require 'gosu'
 class Main
 	def initialize
+		self.caption "Chess Wizard Master"
+
 		@background_image = Gosu::Image.new("Board.jpg")
+	end
+
+	def button_down(id)
+		close if id == Gosu::Kb
 	end
 end
